5a0ca861013bb5042dc34b6ff36adfeed012b34b
Leonardo Miranda <leonardo.miranda@onovomercado.com.br>
2024-05-23 09:43:52 -0300

    fix: Removendo rota não usada de reports

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const pilotsRoutes = require("./routes/pilots.js");
 const contractsRoutes = require("./routes/contracts.js");
 const resourcesRoutes = require("./routes/resources.js");
 const shipsRoutes = require("./routes/ships.js");
-const reportsRoutes = require("./routes/reports.js");
 
 const app = express();
 const PORT = 5000;
@@ -15,7 +14,6 @@ app.use("/pilots", pilotsRoutes);
 app.use("/contracts", contractsRoutes);
 app.use("/resources", resourcesRoutes);
 app.use("/ships", shipsRoutes);
-app.use("/reports", reportsRoutes);
 
 app.get("/", (req, res) => {
   res.send("Hello from homepage");
